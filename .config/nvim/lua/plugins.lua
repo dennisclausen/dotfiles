98129d1bae72f6d1d2f43334480173bb40c1b778
Dr. Dennis Clausen <github@dennis-clausen.de>
2025-08-15 15:52:14 +0000

    adds colorscheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,11 +8,87 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- 3. Plugins mit Lazy laden
 require("lazy").setup({
-  "nvim-lualine/lualine.nvim",
-  "nvim-tree/nvim-tree.lua",
-  "nvim-treesitter/nvim-treesitter",
-  "neovim/nvim-lspconfig",
-  "hrsh7th/nvim-cmp",
-  "lewis6991/gitsigns.nvim",
+
+  -- Catppuccin Theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = false,
+        integrations = {
+          nvimtree = true,
+          treesitter = true,
+          telescope = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- NvimTree
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- Lualine
+  { "nvim-lualine/lualine.nvim" },
+
+  -- Gitsigns
+  { "lewis6991/gitsigns.nvim" },
+
+  -- Autocompletion
+  { "hrsh7th/nvim-cmp" },
+
+  -- LSP Support
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        cmd = { "lua-language-server" },
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            telemetry = { enable = false },
+          },
+        },
+      })
+    end,
+  },
+
+  -- OSC52 Clipboard
+  {
+    "ojroques/nvim-osc52",
+    config = function()
+      local osc52 = require("osc52")
+      osc52.setup()
+      vim.keymap.set("n", "<leader>c", osc52.copy_operator, { expr = true })
+      vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
+      vim.keymap.set("v", "<leader>c", function()
+        vim.cmd("normal! y")
+        osc52.copy_register("\"")
+      end)
+    end,
+  },
+
 })
